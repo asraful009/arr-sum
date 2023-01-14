9f4e6e759c55501df3707be37a720a308e85e858
asraful islam <asraful009@gmail.com>
2023-01-14 14:43:41 +0600

    init run

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const arrSum = (nums) => {
+  if(Array.isArray(nums)) {
+    return nums.reduce((accumulate,  currentValue)=> {
+      return accumulate + currentValue;
+    }, 0);
+  }
   return 0;
 };
 module.exports = arrSum;
